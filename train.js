15139cf881ad40b2ac60b40647ac1d6d88e4bb7d
LeahStep77 <fanaticalbotanical@yahoo.com>
2017-08-13 16:04:23 -0500

    update files to improve

--- train.js
+++ train.js
@@ -1,70 +1,52 @@
-  $(document).ready(function(){
 
-  var config = {
-    apiKey: "AIzaSyDns_oRRc-WQ2ZjLRnb6xyX49ot0QG5ykk",
-    authDomain: "trainschedule-edea3.firebaseapp.com",
-    databaseURL: "https://trainschedule-edea3.firebaseio.com",
-    projectId: "trainschedule-edea3",
-    storageBucket: "",
-    messagingSenderId: "945964018910"
-    };
+//add firebase info
+var config = {
+  apiKey: "AIzaSyDns_oRRc-WQ2ZjLRnb6xyX49ot0QG5ykk",
+  authDomain: "trainschedule-edea3.firebaseapp.com",
+  databaseURL: "https://trainschedule-edea3.firebaseio.com",
+  projectId: "trainschedule-edea3",
+  storageBucket: "trainschedule-edea3.appspot.com",
+  messagingSenderId: "945964018910"
+};
 
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
+//create database variable
+var database = firebase.database();
 
-    // Create a variable to reference the database
-    var database = firebase.database();
+//on submit click function
+$("#addTrain").on("click", function() {
 
-    // Initial Values
-    var trainName = "";
-    var destination = "";
-    var frequency = 0;
-    console.log("FREQUENCY:  " + moment(frequency).format("hh:mm"));
-    var nextArrival = 0;
-    var minAway = 0;
+  //get the info from input areas
+  var trainName = $("#trainName-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var firstTT = moment($("#firstTT-input").val().trim(), "HH:mm").subtract(10, "years").format("X");
+  var frequency = $("#frequency-input").val().trim();      
+   
+  var addTrain = {
+    name: trainName,
+    destination: destination,
+    firstTT: firstTT,
+    frequency: frequency,
+  }
+  //send train info to database 
+  database.ref().push(addTrain);
+  //clear the form
+  $("#trainName-input").val("");
+  $("#destination-input").val("");
+  $("#firstTT-input").val("");
+  $("#frequency-input").val("");
+  return false;
+})
 
-        // Capture Button Click
-    $("#add-train").on("click", function() {
-      // Don't refresh the page!
-      event.preventDefault();
+database.ref().on("child_added", function(snapshot){
+  var trainName = snapshot.val().name;
+  var destination = snapshot.val().destination;
+  var frequency = snapshot.val().frequency;
+  var firstTT = snapshot.val().firstTT;
+  var remainder = moment().diff(moment.unix(firstTT),"minutes")%frequency;
+  var minutes = frequency - remainder;
+  var arrival = moment().add(minutes,"m").format("hh:mm A");
+  //add the train info to the table
+  $("#train-table > tbody").append("<tr><td>" +trainName+ "</td><td>" +destination+ "</td><td>" +frequency+ "</td><td>" + arrival+ "</td><td>" +minutes+ "</td></tr>");
 
-      //logic for storing and retrieving the train.
-      trainName = $("#trainName-input").val().trim();
-      destination = $("#destination-input").val().trim();
-      frequency = $("#frequency-input").val().trim();
-      nextArrival = $("#nextArrival-input").val().trim();
-      minAway = $("#minAway-input").val().trim();
-
-      database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        frequency: frequency,
-        nextArrival: nextArrival,
-        minAway: minAway,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
-
-    });
-
-    // Firebase watcher + initial loader HINT: .on("value")
-    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-      var sv = snapshot.val();
-      // Log everything that's coming out of snapshot
-      console.log(sv.trainName);
-      console.log(sv.destination);
-      console.log(sv.frequency);
-      console.log(sv.nextArrival);
-      console.log(sv.minAway);
-
-//var firstTimeConverted = moment.diff(nextArrival, "hh:mm").subtract(1, "years");
-//console.log(moment.diff(frequency, nextArrival));
-
-console.log(firstTimeConverted);
-      //add train info to the table
-
-      $("#train-table > tbody").append("<tr><td>" + sv.trainName + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td><td>" + sv.nextArrival + "</td><td>" + sv.minAway + "</td></tr>")
-
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
-    });
+    })
